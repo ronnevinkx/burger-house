ad07bca230ed61f62d29007f3c74197b0a0854cc
ronnevinkx <ronnevinkx@gmail.com>
2021-04-14 20:33:58 +0200

    Head update

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-export default function Layout({ children, title, descr }) {
+export default function Layout({ children, meta: { title, descr } }) {
 	return (
 		<>
 			<Head>
